c0cb8fb6bb9ed3c224d497bcd5b29e1e9897920c
spustoff <vyacheslavpustov05@gmail.com>
2023-09-07 20:50:21 +0300

    Initial Commit

---
+++ App187/App187App.swift
@@ -0,0 +1,17 @@
+//
+//  App187App.swift
+//  App187
+//
+//  Created by Вячеслав on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct App187App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App187/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  App187
+//
+//  Created by Вячеслав on 9/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
